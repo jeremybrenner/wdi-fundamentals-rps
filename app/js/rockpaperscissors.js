21d61218f6595d1c43699d2d7bfc3b6acb324bb1
jmbrenne <jeremybrenner@Jeremys-MacBook-Air.local>
2015-04-24 18:24:20 -0700

    WDI SF 2015-04-27 :: Jeremy Brenner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,16 +22,10 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
     return move || getInput();
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
     return move || randomPlay();
 }
 
@@ -40,17 +34,17 @@ function getWinner(playerMove,computerMove) {
       if ( playerMove === computerMove) {
             winner = 'tie'
         } else if ((playerMove === 'rock') && (computerMove === 'paper')) {
-            winner = 'computer'
+            winner = 'computer';
         } else if ((playerMove === 'rock') && (computerMove === 'scissors')) {
-            winner = 'player'
+            winner = 'player';
         } else if ((playerMove === 'paper') && (computerMove === 'rock')) {
-            winner = 'player'
+            winner = 'player';
         } else if ((playerMove === 'paper') && (computerMove === 'scissors')) {
-            winner = 'computer'
+            winner = 'computer';
         } else if ((playerMove === 'scissors') && (computerMove === 'rock')) {
-            winner = 'computer'
+            winner = 'computer';
         } else if ((playerMove === 'scissors') && (computerMove === 'paper')) {
-            winner = 'player'
+            winner = 'player';
         }    
 
         ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -76,8 +70,6 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
     while (playerWins < 5 && computerWins < 5) {
         var playerMove = getPlayerMove();
         var computerMove = getComputerMove();
@@ -101,7 +93,43 @@ function playToFive() {
         console.log ( 'Computer: ' + computerWins); 
         console.log ('');
     }
-    (playerWins === 5)? console.log("You win!"):console.log("Computer Wins!");
+    (playerWins === 5)? console.log("You win the game!"):console.log("Computer wins the game!");
     return [playerWins, computerWins];
 }
-playToFive()
+playToFive();
+
+///////////////////////////////////////////////
+/*function to allow play to 'x' instead of 5 */
+///////////////////////////////////////////////
+
+/*function playTo(x) {
+    console.log("Let's play Rock, Paper, Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
+    while (playerWins < x && computerWins < x) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove, computerMove);
+        if ( winner === 'player') {
+            console.log ('Player chose ' + playerMove + ', and Computer chose ' + computerMove);
+            console.log ('Player Wins!');
+            playerWins +=1;
+            }
+        else if ( winner === 'computer'){
+            console.log ('Computer chose ' + computerMove + ', and Player chose ' + playerMove);
+            console.log ('Computer Wins!');
+            computerWins +=1;
+            }
+        else {
+            console.log ('Its a tie, no winner!');
+        }
+        console.log ('');
+        console.log ('<<< Scoreboard >>>');
+        console.log ( 'Player: ' + playerWins);
+        console.log ( 'Computer: ' + computerWins); 
+        console.log ('');
+    }
+    (playerWins === x)? console.log("You win the game!"):console.log("Computer wins the game!");
+    return [playerWins, computerWins];
+}
+playTo();*/
